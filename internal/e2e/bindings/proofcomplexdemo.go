4d9589d87fb84c4a8484fd64d9b7c204251a00dd
islishude <islishude@gmail.com>
2026-08-01 09:00:33 +0800

    Update binding

--- internal/e2e/bindings/proofcomplexdemo.go
+++ internal/e2e/bindings/proofcomplexdemo.go
@@ -4,9 +4,11 @@
 package bindings
 
 import (
+	"context"
 	"errors"
 	"math/big"
 	"strings"
+	"time"
 
 	ethereum "github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
@@ -27,6 +29,8 @@ var (
 	_ = types.BloomLookup
 	_ = event.NewSubscription
 	_ = abi.ConvertType
+	_ = time.Tick
+	_ = context.Background
 )
 
 // ProofComplexDemoMetaData contains all meta data concerning the ProofComplexDemo contract.
